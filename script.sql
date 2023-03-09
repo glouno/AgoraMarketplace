e968f69a0617605f3e0adcd2cde6661338b779d6
yezhan <119345588+yezhan14promax@users.noreply.github.com>
2023-03-09 19:19:10 +0100

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,15 @@
+create table produits
+(
+    id           int auto_increment
+        primary key,
+    nom          varchar(100)   not null,
+    prix         int            not null,
+    types        varchar(20)    null,
+    descriptions varchar(10000) null,
+    type_prix    varchar(20)    null,
+    image        varchar(1000)  null,
+    constraint id
+        unique (id)
+);
+
+
